4e23a75d7da04ae2ff5c895ff1b2e8ca54def60a
ayhon <43295942+ayhon@users.noreply.github.com>
2023-06-30 20:08:23 +0200

    Show something onto the screen

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 use bevy::input::mouse::{MouseButtonInput, MouseMotion, MouseWheel};
+#[cfg(feature = "debug")]
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 mod minesweeper_model;
 // use minesweeper_model::*;
 
@@ -7,31 +9,36 @@ mod minesweeper_model;
 struct MineCell(usize, usize);
 
 fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_system(handle_mouse_clicks)
-        // .add_system(print_mouse_events_system)
-        .add_startup_system(setup)
-        .run()
+    let mut app = App::new();
+    app.add_plugins(DefaultPlugins);
+    app.add_system(handle_mouse_clicks);
+    #[cfg(feature = "debug")]
+    // Debug hierarchy inspector
+    app.add_plugin(WorldInspectorPlugin::new());
+    // .add_system(print_mouse_events_system)
+    app.add_startup_system(setup);
+    app.run();
 }
 
 const CELL_WIDTH: f32 = 20f32;
 const CELL_HEIGHT: f32 = CELL_WIDTH;
 
 pub fn setup(mut commands: Commands) {
-    commands.spawn(Camera2d::default());
+    commands.spawn(Camera2dBundle::default());
     for x in 0..minesweeper_model::GRID_SIZE.0 {
         for y in 0..minesweeper_model::GRID_SIZE.0 {
+            let size = Vec2::new(CELL_WIDTH,CELL_HEIGHT);
+            let pos =  ((x as f32)*CELL_WIDTH, (y as f32)*CELL_HEIGHT);
             commands.spawn(
                 (
                     MineCell(x,y),
                     SpriteBundle {
                         sprite: Sprite {
                             color: Color::YELLOW,
-                            custom_size: Some(Vec2::new(CELL_WIDTH,CELL_HEIGHT)),
+                            custom_size: Some(size),
                             ..Default::default()
                         },
-                        transform: Transform::from_xyz((x as f32)*CELL_WIDTH, (y as f32)*CELL_HEIGHT, 0.0),
+                        transform: Transform::from_xyz(pos.0, pos.1, 0.0),
                         ..Default::default()
                     }
                 )
